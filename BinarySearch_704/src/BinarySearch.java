369bfc70f463721ce6947938f606a005cdc92a7d
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-04-05 21:12:00 -0500

    removed errors

--- BinarySearch_704/src/BinarySearch.java
+++ BinarySearch_704/src/BinarySearch.java
@@ -1,13 +1,12 @@
 //Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 //You must write an algorithm with O(log n) runtime complexity.
 public class BinarySearch {
-    public:
-    int search(vector<int>& nums, int target) {
-        if (nums.size() == 0) {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) {
             return -1;
         }
         int left = 0;
-        int right = nums.size() - 1;
+        int right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
--- FirstBadVersion_278/src/FirstBadVersion.java
+++ FirstBadVersion_278/src/FirstBadVersion.java
@@ -37,6 +37,3 @@ public class FirstBadVersion {
         return true;
     }
 }
-
-    }
-}
